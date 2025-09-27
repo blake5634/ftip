d39ebecf475ddb5260cc8886b0353b63bc244cb5
Blake Hannaford <blake.hannaford@gmail.com>
2025-09-27 11:09:38 -0700

    new cl option to skip menu

--- ftip.py
+++ ftip.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-# import os
-# import re
-# import subprocess as sub
-# import sys
-import argparse as ap
+import sys
 import pyperclip
+import subprocess
 
 
 def process(n,t,v):
@@ -13,6 +10,8 @@ def process(n,t,v):
     print('    type:',t[n])
     print('    val: \n',v[n])
     if t[n] == 'link'  or t[n] == 'txt':
+        # Optional: Show notification that it worked
+        subprocess.run(['notify-send', 'Copied to clipboard', v[n] ])
         pyperclip.copy(v[n])
         quit
     elif t[n] == 'txtfile':
@@ -22,6 +21,8 @@ def process(n,t,v):
         for line in ifp:
             res += line
         pyperclip.copy(res)
+        # Optional: Show notification that it worked
+        subprocess.run(['notify-send', 'Copied to clipboard', res] , stderr=subprocess.DEVNULL )
         quit()
 
 
@@ -29,6 +30,9 @@ def process(n,t,v):
 fn = '/home/blake/BH_Sync_New/Maintenance/ftip/ftipDB.txt'
 fp = open(fn, 'r')
 
+#
+#  get setup from the database
+#
 names = []
 types = {}
 vals = {}
@@ -41,15 +45,35 @@ for line in fp:
     types[n] = t
     vals[n] = v
 
-i = 1
-print('\n      Select:')
-for n in names:
-    print(f'{i:2}  {n:15} ')
-    i += 1
-
-sel = input ('> ')
-if sel=='':
+#
+#  look at command line
+#
+args = sys.argv
+if len(args) == 2:
+    if args[1] not in names:
+        print(f"I don't understand {args[1]} - quitting")
+        quit()
+    else:
+        #
+        #  go ahead and load paste buff
+        process(args[1], types, vals)
     quit()
+elif len(args) == 1:
+    #
+    #  print menu for user
+    #
+    i = 1
+    print('\n      Select:')
+    for n in names:
+        print(f'{i:2}  {n:15} ')
+        i += 1
+
+    sel = input ('> ')
+    if sel=='':
+        quit()
+    else:
+        nn = int(sel)-1
+        process(names[nn],types,vals)
 else:
-    nn = int(sel)-1
-    process(names[nn],types,vals)
+    print("I couldn't understand the command line: ", ' '.join(args))
+    quit()
